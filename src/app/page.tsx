b0dc471b0db9b978a9ddd7c450564ce61eca5f83
MS_btff <whdghks845@gmail.com>
2024-06-19 16:41:17 +0900

    feat 공통컴포넌트트 회원탈퇴 작업

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
 import LargeCard from "@/components/Card/LargeCard";
+import Withdrawal from "@/components/Register/Withdrawal";
 
 export default function Home() {
   return (
     <>
       <div className="flex flex-col">
-        <LargeCard />
+        <Withdrawal />
       </div>
     </>
   );

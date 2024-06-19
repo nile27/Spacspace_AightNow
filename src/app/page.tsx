62678f8c054ea3654a228d77c60b81781811597f
MS_btff <whdghks845@gmail.com>
2024-06-19 16:13:17 +0900

    feat 공통컴포넌트트card 작업

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
-import Card from "@/components/Card/Card";
+import LargeCard from "@/components/Card/LargeCard";
 
 export default function Home() {
   return (
     <>
       <div className="flex flex-col">
-        <Card />
+        <LargeCard />
       </div>
     </>
   );

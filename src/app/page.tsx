c6e813fad1a2770d4d915089b30c80daf7ca8560
MS_btff <whdghks845@gmail.com>
2024-06-19 18:13:27 +0900

    feat 공통컴포넌트트 로고 작업

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
+import Stock from "@/components/Stock/Stock";
 import Toggle from "@/components/Toggle/Toggle";
 
 export default function Home() {
   return (
     <>
       <div className="flex flex-col">
-        <Toggle />
+        <Stock />
       </div>
     </>
   );
--- src/components/Toggle/Toggle.tsx
+++ src/components/Toggle/Toggle.tsx
@@ -16,17 +16,19 @@ export default function Toggle() {
         >
           <button
             className={`${
-              toggle ? "text-neutral-400" : "text-zinc-700 bg-white w-8 h-8 px-1.5 rounded"
-            } text-xl font-normal font-['Pretendard'] leading-7`}
+              toggle
+                ? "text-neutral-400"
+                : "text-zinc-700 bg-white w-8 h-8 px-1.5 rounded font-bold"
+            } text-xl  font-['Pretendard'] leading-7`}
           >
             $
           </button>
         </div>
-        <button className="w-8 h-8 px-1.5  rounded flex-col justify-center items-center gap-2.5 inline-flex">
+        <button className="w-8 h-8 p-1.5  rounded flex-col justify-center items-center gap-2.5 inline-flex">
           <div
             className={`${
-              toggle ? "text-zinc-700 bg-white w-8 h-8 px-1.5 rounded" : "text-neutral-400"
-            } text-xl font-bold font-['Pretendard'] leading-7`}
+              toggle ? "text-zinc-700 bg-white w-8 h-8  rounded  font-bold" : "text-neutral-400"
+            } text-xl  font-['Pretendard'] leading-7`}
           >
             원
           </div>

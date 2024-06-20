f55a3ebaa9b0115938444a75387c3c1f7312cab5
MS_btff <whdghks845@gmail.com>
2024-06-20 09:59:55 +0900

    style 공통컴포넌트트 스타일 작업

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,29 +1,7 @@
-import Card from "@/components/Card/Card";
-import CardTitle from "@/components/Card/CardTitle";
-import LargeCard from "@/components/Card/LargeCard";
-import WatchCard from "@/components/Card/WatchCard";
-import Dropdowntset from "@/components/Dropdown/Dropdowntset";
-import ListNews from "@/components/List/ListNews";
-import ListStockDown from "@/components/List/ListStockDown";
-import ListStockUp from "@/components/List/ListStockUp";
-import Stock from "@/components/Stock/Stock";
-import Toggle from "@/components/Toggle/Toggle";
-
 export default function Home() {
   return (
     <>
-      <div className="w-screen h-screen flex flex-col justify-center items-center gap-8">
-        <Dropdowntset />
-        <Stock />
-        <Toggle />
-        <Card />
-        <CardTitle />
-        <LargeCard />
-        <WatchCard />
-        <ListNews />
-        <ListStockUp />
-        <ListStockDown />
-      </div>
+      <div className="w-screen h-screen flex flex-col justify-center items-center gap-8"></div>
     </>
   );
 }

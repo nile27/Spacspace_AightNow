bffccd94b8ddded8750cb8ef4856a1b6883025c3
MS_btff <whdghks845@gmail.com>
2024-06-21 10:01:51 +0900

    style report 스타일 작업

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,10 @@
-import Badge from "@/components/Badge/Badge";
-import Checkbox from "@/components/Checkbox/Checkbox";
-import { BadgeBlack, BadgeBlue, BadgeGray } from "@/components/Badge/Badge";
+import Stock from "@/components/Stock/Stock";
+import Report from "@/features/report/components/Report";
 
 export default function Home() {
   return (
     <>
-      <BadgeBlue />
-      <Checkbox />
+      <Report />
     </>
   );
 }

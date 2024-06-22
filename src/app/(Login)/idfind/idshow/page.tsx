115117bc18be0a933d6fbac46a7d757ed838d1ff
nile27 <ddj03104@gmail.com>
2024-06-22 16:12:00 +0900

    feat: idFind Link

--- src/app/(Login)/idfind/idshow/page.tsx
+++ src/app/(Login)/idfind/idshow/page.tsx
@@ -3,6 +3,7 @@ import Google from "@/../public/icons/Google.svg";
 import Kakao from "@/../public/icons/KakaoTalk.svg";
 import Apple from "@/../public/icons/Apple.svg";
 import Naver from "@/../public/icons/Naver.svg";
+import Link from "next/link";
 
 import { cn } from "@/lib/utils";
 import { cva } from "class-variance-authority";
@@ -11,7 +12,7 @@ type TLogotype = {
   style?: "apple" | "kakao" | "naver" | "google" | undefined;
 };
 
-export default function IdFetch({ style }: TLogotype) {
+export default function IdShow({ style }: TLogotype) {
   const IconRenderIcon = (icon: "apple" | "kakao" | "naver" | "google" | undefined) => {
     switch (icon) {
       case "kakao":
@@ -44,25 +45,30 @@ export default function IdFetch({ style }: TLogotype) {
   });
 
   return (
-    <div className="flex justify-center items-center flex-col gap-10 max-w-[386px] ">
-      <div className="flex justify-center items-center flex-col gap-5 w-full h-auto mb-10 ">
-        <span className="mt-3">휴대번호와 일치하는 아이디입니다.</span>
-        <div className="w-full h-auto p-4 flex justify-center items-center flex-col rounded-lg border-[1px] gap-4 border-scaleGray-300 ">
-          <div className="flex justify-center items-center gap-2">
-            <span>아이디:</span>
-            <div className={cn(buttonVariants({ style }), ` `)}>
-              {style && IconRenderIcon(style)}
-            </div>
+    <>
+      <h1 className="  text-h3 font-extrabold">아이디 찾기</h1>
+      <div className="flex justify-center items-center flex-col gap-10 max-w-[386px] ">
+        <div className="flex justify-center items-center flex-col gap-5 w-full h-auto mb-[30px] ">
+          <span className="mt-3">휴대번호와 일치하는 아이디입니다.</span>
+          <div className="w-full h-auto p-4 flex justify-center pl-[100px] items-start flex-col rounded-lg border-[1px] gap-4 border-scaleGray-300 ">
+            <div className="flex justify-start items-center gap-2 ">
+              <span>아이디:</span>
+              <div className={cn(buttonVariants({ style }), ` `)}>
+                {style && IconRenderIcon(style)}
+              </div>
 
-            <span>asjkdnajsknd</span>
-          </div>
-          <div className="flex justify-center items-center gap-2">
-            <span>가입일:</span>
-            <span>2023.01.04</span>
+              <span>asjkdnajsknd</span>
+            </div>
+            <div className="flex justify-start items-center gap-2">
+              <span>가입일:</span>
+              <span>2023.01.04</span>
+            </div>
           </div>
         </div>
+        <Link href={"/login"} className="w-full h-auto">
+          <TextButton>로그인하기</TextButton>
+        </Link>
       </div>
-      <TextButton>로그인하기</TextButton>
-    </div>
+    </>
   );
 }

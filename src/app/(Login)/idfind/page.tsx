d49c4576d68a68e4a9e14b09f53de563dfb1856b
nile27 <ddj03104@gmail.com>
2024-06-22 16:12:00 +0900

    feat: idFind Link

--- src/app/(Login)/idfind/page.tsx
+++ src/app/(Login)/idfind/page.tsx
@@ -2,56 +2,46 @@
 import { useState } from "react";
 import InputExtends from "@/components/Input/Input";
 import TextButton from "@/components/btnUi/TextButton";
-import IdFetch from "./component/IdFetch";
+import Link from "next/link";
 
 export default function IdFind() {
   const [nameText, setName] = useState("");
-  const [idText, setId] = useState("");
   const [phone, setPhone] = useState("");
   return (
     <>
       <h1 className="  text-h3 font-extrabold">아이디 찾기</h1>
-      {1 ? (
-        <form className="mt-10 w-full h-auto flex flex-col gap-4 mb-10">
-          <div className="w-full h-auto flex flex-col gap-4 mb-[56px]">
-            <InputExtends
-              id="username"
-              placeholder="이름을 입력해주세요"
-              label="이름"
-              value={nameText}
-              autoComplete="username"
-              onChange={e => setName(e.target.value)}
-            />
-            <InputExtends
-              id="id"
-              type="text"
-              placeholder="아이디를 입력해주세요"
-              label="아이디"
-              value={idText}
-              autoComplete="username"
-              onChange={e => setId(e.target.value)}
-            />
-            <InputExtends
-              type="tel"
-              id="tel"
-              label="전화번호"
-              value={phone}
-              placeholder="-를 제외한 휴대폰번호를 입력해주세요."
-              onChange={e => setPhone(e.target.value)}
-            />
-          </div>
 
-          {idText && phone && nameText ? (
+      <form className="mt-10 w-full h-auto flex flex-col gap-4 mb-10">
+        <div className="w-full h-auto flex flex-col gap-4 mb-[30px]">
+          <InputExtends
+            id="username"
+            placeholder="이름을 입력해주세요"
+            label="이름"
+            value={nameText}
+            autoComplete="username"
+            onChange={e => setName(e.target.value)}
+          />
+
+          <InputExtends
+            type="tel"
+            id="tel"
+            label="전화번호"
+            value={phone}
+            placeholder="-를 제외한 휴대폰번호를 입력해주세요."
+            onChange={e => setPhone(e.target.value)}
+          />
+        </div>
+
+        {phone && nameText ? (
+          <Link className="w-full h-auto" href={"/idfind/idshow"}>
             <TextButton size="full">아이디 찾기</TextButton>
-          ) : (
-            <TextButton size="full" color="disable">
-              아이디 찾기
-            </TextButton>
-          )}
-        </form>
-      ) : (
-        <IdFetch />
-      )}
+          </Link>
+        ) : (
+          <TextButton size="full" color="disable">
+            아이디 찾기
+          </TextButton>
+        )}
+      </form>
     </>
   );
 }

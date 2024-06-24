9a0ba8e1aeb479a5d11e504168e3556e817e6e51
MS_btff <whdghks845@gmail.com>
2024-06-24 10:11:06 +0900

    style report 스타일 작업

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,7 @@
-import GuestHeader from "@/components/Main/GuestHeder";
-import Report from "../features/report/components/Report";
-import StockApi from "@/features/report/components/api/StockApi";
-
 export default function Home() {
   return (
     <>
-      <div className="w-[1440px] h-full bg-[#f1f3f8] p-28 ">
-        <GuestHeader />
-        <Report />
-
-        <StockApi />
-      </div>
+      <div className="w-[1440px] h-full bg-[#f1f3f8]"></div>
     </>
   );
 }

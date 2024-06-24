b9a4fe27509b3d503496d2fa5d14a141f0a429d8
MS_btff <whdghks845@gmail.com>
2024-06-24 13:32:41 +0900

    fix svg sprite 작업

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,3 @@
-import GuestHeader from "@/components/Main/GuestHeder";
-import Report from "../features/report/components/Report";
-import StockApi from "@/features/report/components/api/StockApi";
 import Icons from "@/components/Icon/Icons";
 
 export default function Home() {

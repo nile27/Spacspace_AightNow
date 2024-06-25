fe711fef61f0bf513997c964a39e50d69af57f5d
MS_btff <whdghks845@gmail.com>
2024-06-25 09:44:00 +0900

    style news 작업

--- src/app/(News)/news/page.tsx
+++ src/app/(News)/news/page.tsx
@@ -8,13 +8,15 @@ export default function News() {
     <>
       <Header />
 
-      <div className="w-[1200px] h-full font-pretendard mt-48">
+      <div className="w-[1200px] h-full font-pretendard mt-60">
         <FavoriteNews />
-        <h2 className="font-bold text-2xl py-4">관심종목과 관련된 뉴스</h2>
-        <div className="grid grid-cols-3 gap-y-4">
-          <Card />
-          <Card />
-          <Card />
+        <div className="my-12">
+          <h2 className="font-bold text-2xl py-4">관심종목과 관련된 뉴스</h2>
+          <div className="grid grid-cols-3 gap-5">
+            <Card />
+            <Card />
+            <Card />
+          </div>
         </div>
         <h2 className="font-bold text-2xl py-4">최신뉴스</h2>
         <div className="w-[1200px] h-[940px] bg-white rounded-2xl p-12">

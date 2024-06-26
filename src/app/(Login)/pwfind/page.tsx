2ec2d441f657d5c04d6065ba391824f4681c03b1
nile27 <ddj03104@gmail.com>
2024-06-26 10:11:48 +0900

    feat: 비밀번호 찾기 유효성 검사

--- src/app/(Login)/pwfind/page.tsx
+++ src/app/(Login)/pwfind/page.tsx
@@ -3,12 +3,26 @@ import { useState } from "react";
 import NewInput from "@/components/Input/NewInput";
 import TextButton from "@/components/btnUi/TextButton";
 import AuthModal from "./component/AuthModal";
+import { useRouter } from "next/navigation";
 
-export default function IdFind() {
-  const [nameText, setName] = useState("");
-  const [idText, setId] = useState("");
-  const [email, setEmail] = useState("");
+export default function Pwfind() {
+  const [inputText, setInput] = useState({
+    name: "",
+    id: "",
+    email: "",
+  });
   const [ismodal, setModal] = useState(false);
+  const [err, setErr] = useState(true);
+  const handleInputValue = (key: string) => (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInput({ ...inputText, [key]: e.target.value });
+  };
+  const handleOnClick = () => {
+    if (inputText.email.length < 3) {
+      setErr(false);
+      return;
+    }
+    setModal(!ismodal);
+  };
   return (
     <>
       <h1 className="  text-h3 font-extrabold">비밀번호 찾기</h1>
@@ -19,8 +33,9 @@ export default function IdFind() {
             id="username"
             placeholder="이름을 입력해주세요"
             label="이름"
-            value={nameText}
-            onChange={e => setName(e.target.value)}
+            value={inputText.name}
+            style={!err ? "error" : undefined}
+            onChange={handleInputValue("name")}
             autoComplete="username"
           />
           <NewInput
@@ -28,22 +43,25 @@ export default function IdFind() {
             type="text"
             placeholder="아이디를 입력해주세요"
             label="아이디"
-            value={idText}
-            onChange={e => setId(e.target.value)}
-            autoComplete="username"
+            value={inputText.id}
+            style={!err ? "error" : undefined}
+            onChange={handleInputValue("id")}
+            autoComplete="none"
           />
           <NewInput
             type="text"
             id="email"
             label="이메일주소"
             placeholder="가입 시 입력한 이메일주소를 입력해주세요."
-            value={email}
-            onChange={e => setEmail(e.target.value)}
+            value={inputText.email}
+            style={!err ? "error" : undefined}
+            onChange={handleInputValue("email")}
+            caption={!err ? "*  등록되지 않은 회원이거나 잘못된 회원정보입니다." : undefined}
           />
         </div>
 
-        {idText && email && email.includes("@") && nameText ? (
-          <TextButton onClick={() => setModal(!ismodal)} size="full">
+        {inputText.id && inputText.email && inputText.email.includes("@") && inputText.name ? (
+          <TextButton onClick={handleOnClick} size="full">
             임시 비밀번호 발급
           </TextButton>
         ) : (

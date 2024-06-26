52a83bdb65d34951e72169c1754c9060358ee13e
ur <dorosi150@gmail.com>
2024-06-26 09:56:29 +0900

    style: 더보기 기능 grid로 수정

--- src/features/search/components/StockList.tsx
+++ src/features/search/components/StockList.tsx
@@ -11,24 +11,13 @@ export default function StockList({ items }: { items: any[] }) {
     <>
       <div className="p-6 bg-white rounded-2xl flex-col justify-start items-start flex w-full">
         <div className="flex flex-col justify-start items-center gap-4 w-full">
-          {Array.from({ length: Math.ceil(visibleItems / 6) }).map((_, i) => (
-            <div key={i} className="flex justify-start items-start gap-4 w-full">
-              <div className="flex-col justify-start items-start gap-4 inline-flex w-1/2">
-                {items.slice(i * 6, i * 6 + 3).map((data, index) => (
-                  <div key={index} className="flex justify-between items-center rounded-lg">
-                    <Stock key={index} logo={"apple"} gap="gap-[64px]" />
-                  </div>
-                ))}
+          <div className="grid grid-cols-2 gap-4 w-full">
+            {items.slice(0, visibleItems).map((data, index) => (
+              <div key={index} className="flex justify-between items-center rounded-lg">
+                <Stock key={index} logo={"apple"} gap="gap-[64px]" />
               </div>
-              <div className="flex-col justify-start items-start gap-4 inline-flex w-1/2">
-                {items.slice(i * 6 + 3, i * 6 + 6).map((data, index) => (
-                  <div key={index} className="flex justify-between items-center rounded-lg">
-                    <Stock key={index} logo={"apple"} gap="gap-[64px]" />
-                  </div>
-                ))}
-              </div>
-            </div>
-          ))}
+            ))}
+          </div>
           {visibleItems < items.length && (
             <div className="h-12 pt-2 flex justify-center items-center gap-2.5 w-full border-t-2">
               <button

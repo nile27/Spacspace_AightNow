3cdd9f851535bdb755f85cbecb0dcae71991c9e8
MS_btff <whdghks845@gmail.com>
2024-06-27 17:41:52 +0900

    watchlist css 작업

--- src/app/(watchlist)/watchlist/page.tsx
+++ src/app/(watchlist)/watchlist/page.tsx
@@ -1,11 +1,13 @@
 import Header from "@/components/Header";
-import Watchlist from "@/features/Watchlist/components/watchlist";
+import Watchlist from "@/features/Watchlist/components/Watchlist";
 
 export default function page() {
   return (
     <>
       <Header />
-      <Watchlist />
+      <div className="w-full h-full bg-scaleGray-400 opacity-90 flex justify-center items-center">
+        <Watchlist />
+      </div>
     </>
   );
 }
--- src/features/Watchlist/components/Watchlist.tsx
+++ src/features/Watchlist/components/Watchlist.tsx
@@ -1,49 +1,16 @@
-"use client";
-
-import WatchCard from "@/components/Card/WatchCard";
-import BasicIcon from "@/components/Icon/BasicIcons";
 import StockList from "@/components/Stock/StockList";
-import { useState } from "react";
+import WatchInput from "./WatchInput";
 
 export default function Watchlist() {
-  const [isShow, setIsShow] = useState(false);
-  const [search, setSearch] = useState("");
-
-  const searchHandle = (e: React.ChangeEvent<HTMLInputElement>) => {
-    e.preventDefault();
-    setSearch(e.target.value);
-    console.log(search);
-  };
-
   return (
     <>
       <div className="w-[794px] h-[735px] bg-white font-pretendard p-10 rounded-[32px]">
         <div className="font-bold text-2xl text-mainNavy-900 text-center">관심종목 추가</div>
         <div>
-          <form action="">
-            <div className="flex items-center">
-              <input
-                type="text"
-                placeholder="검색어를 입력해주세요."
-                onChange={searchHandle}
-                className="w-[714px] h-14 rounded-lg border border-scaleGray-400 relative p-2 mb-6 mt-8"
-              />
-              <button className="absolute mt-2 2xl:right-[630px] xl:right-[380px] ">
-                <BasicIcon name="Search" size={24} />
-              </button>
-            </div>
-          </form>
+          <WatchInput />
         </div>
-        <div className="w-full flex flex-col  font-['pretendard']">
-          <div className="flex justify-between">
-            <h2>최근 검색한 종목</h2>
-            <div>전체삭제</div>
-          </div>
-          <div>
-            <WatchCard />
-          </div>
-        </div>
-        <div className="w-[714px] h-[332px]">
+
+        <div className="w-[714px] h-[332px] mt-12">
           <h2 className="text-lg text-mainNavy-900">인기검색어</h2>
           <div className="w-[714px] h-[288px] flex justify-center items-center gap-6  border border-scaleGray-400 rounded-2xl ">
             <div>

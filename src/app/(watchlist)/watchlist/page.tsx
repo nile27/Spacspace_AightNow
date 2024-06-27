09479af558f27794f6f217abfcf958af1f199649
MS_btff <whdghks845@gmail.com>
2024-06-27 10:01:11 +0900

    watchlist css 작업

--- src/app/(watchlist)/watchlist/page.tsx
+++ src/app/(watchlist)/watchlist/page.tsx
@@ -1,36 +1,35 @@
 import Header from "@/components/Header";
 import BasicIcon from "@/components/Icon/BasicIcons";
-import Icon from "@/components/Stock/Icon";
+import StockList from "@/components/Stock/StockList";
 
 export default function page() {
   return (
     <>
       <Header />
-      <div>
+      <div className="w-[794px] h-[571px] bg-white font-pretendard p-10 rounded-[32px]">
         <div className="">관심종목 추가</div>
         <div>
           <form action="">
-            <input
-              type="text"
-              placeholder="검색어를 입력해주세요."
-              className="w-[714px] h-14"
-            ></input>
-            <BasicIcon name="Search" size={24} />
+            <div className="flex items-center">
+              <input
+                type="text"
+                placeholder="검색어를 입력해주세요."
+                className="w-[714px] h-14 rounded-lg border border-gray-400"
+              />
+              <button className="relative right-8 ">
+                <BasicIcon name="Search" size={24} />
+              </button>
+            </div>
           </form>
         </div>
         <div className="w-[714px] h-[332px]">
           <h2>인기검색어</h2>
           <div className="w-[714px] h-[288px]">
-            <div className="w-[323px] h-8  flex gap-2 mt-5 item-center">
-              <Icon name="apple" size={32} />
-              <div className=" flex justify-between items-center gap-1 font-medium ">
-                <span>애플</span>
-                <span>∙</span>
-                <span>$00.00</span>
-                <span className="text-rose-500">▲+1.75</span>
-                <span className="text-rose-500">+0.82%</span>
-              </div>
-            </div>
+            <StockList name={"apple"} />
+            <StockList name={"apple"} />
+            <StockList name={"apple"} />
+            <StockList name={"apple"} />
+            <StockList name={"apple"} />
           </div>
         </div>
       </div>

209aa0bdb14897fa8e31e2c6df9e5675bce2e3c9
MS_btff <whdghks845@gmail.com>
2024-06-27 16:28:27 +0900

    watchlist css 작업

--- src/components/Stock/StockList.tsx
+++ src/components/Stock/StockList.tsx
@@ -2,19 +2,20 @@ import Icon from "./Icon";
 
 type TStockList = {
   name: string;
+  size: number;
 };
 
-export default function StockList({ name }: TStockList) {
+export default function StockList({ name, size }: TStockList) {
   return (
     <>
       <div className="w-[323px] h-8  flex gap-2 mt-5 item-center">
         <Icon name={name} size={32} />
-        <div className=" flex justify-between items-center gap-1 font-medium ">
+        <div className="w-[290px] flex justify-between items-center  font-medium ">
           <span>애플</span>
-          <span>∙</span>
-          <span>$00.00</span>
-          <span className="text-rose-500">▲+1.75</span>
-          <span className="text-rose-500">+0.82%</span>
+          <div>
+            <span className="text-rose-500">▲+1.75</span>
+            <span className="text-rose-500">+0.82%</span>
+          </div>
         </div>
       </div>
     </>

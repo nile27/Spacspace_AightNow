bbee8c23d865a1b9e0799ac40d5f05cac045865d
MS_btff <whdghks845@gmail.com>
2024-06-28 09:53:30 +0900

    watchlist css 작업

--- src/features/Watchlist/components/Watchlist.tsx
+++ src/features/Watchlist/components/Watchlist.tsx
@@ -12,20 +12,20 @@ export default function Watchlist() {
 
         <div className="w-[714px] h-[332px] mt-12">
           <h2 className="text-lg text-mainNavy-900">인기검색어</h2>
-          <div className="w-[714px] h-[288px] flex justify-center items-center gap-6  border border-scaleGray-400 rounded-2xl ">
+          <div className="w-[714px] h-[288px] flex justify-center items-center gap-6  border border-scaleGray-400 rounded-2xl s ">
             <div>
               <StockList name={"apple"} size={32} />
-              <StockList name={"apple"} size={32} />
-              <StockList name={"apple"} size={32} />
-              <StockList name={"apple"} size={32} />
-              <StockList name={"apple"} size={32} />
+              <StockList name={"google"} size={32} />
+              <StockList name={"amazon"} size={32} />
+              <StockList name={"unity"} size={32} />
+              <StockList name={"tesla"} size={32} />
             </div>
             <div>
               <StockList name={"apple"} size={32} />
-              <StockList name={"apple"} size={32} />
-              <StockList name={"apple"} size={32} />
-              <StockList name={"apple"} size={32} />
-              <StockList name={"apple"} size={32} />
+              <StockList name={"google"} size={32} />
+              <StockList name={"amazon"} size={32} />
+              <StockList name={"unity"} size={32} />
+              <StockList name={"tesla"} size={32} />
             </div>
           </div>
         </div>

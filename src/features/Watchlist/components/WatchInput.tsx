f97bc770c963457d3f6c0bc605b48c0b9f52decd
MS_btff <whdghks845@gmail.com>
2024-06-28 09:31:28 +0900

    watchlist css 작업

--- src/features/Watchlist/components/WatchInput.tsx
+++ src/features/Watchlist/components/WatchInput.tsx
@@ -2,7 +2,7 @@
 import BasicIcon from "@/components/Icon/BasicIcons";
 import { useEffect, useRef, useState } from "react";
 
-const stockName = ["apple", "google", "tesla"];
+const stockName = ["애플", "구글", "테슬라", "아마존", "마이크로소프트", "유니티"];
 
 export default function WatchInput() {
   const [isShow, setIsShow] = useState(false);
@@ -42,16 +42,16 @@ export default function WatchInput() {
   return (
     <>
       <form action="">
-        <div className="flex items-center ">
+        <div className="w-[734px] flex items-center ">
           <input
             type="text"
             placeholder="검색어를 입력해주세요."
             value={search}
             onKeyDown={handleKeyDown}
             onChange={searchHandle}
-            className="w-[712px] h-14 rounded-lg border border-scaleGray-400 relative p-2 mb-6 mt-8"
+            className="w-[734px] h-14 rounded-lg border border-scaleGray-400 relative p-2 mb-6 mt-8"
           />
-          <button className="absolute mt-2 2xl:right-[630px] xl:right-[380px] ">
+          <button className="sticky mt-2 right-6 ">
             <BasicIcon name="Search" size={24} />
           </button>
         </div>
@@ -61,7 +61,7 @@ export default function WatchInput() {
             {filteredStocks.map((stock, index) => (
               <li
                 key={stock}
-                className={`p-2 hover:bg-gray-100 cursor-pointer flex gap-4 items-center ${
+                className={`p-2 hover:bg-gray-100 cursor-pointer flex gap-4 items-center rounded-lg ${
                   index === selectedIndex ? "bg-gray-200" : ""
                 }`}
                 onClick={() => handleItemClick(stock)}

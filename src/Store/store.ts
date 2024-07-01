eef5fb8d1e8bda1c1a60bdb1d6e380b09e7d9bcc
MS_btff <whdghks845@gmail.com>
2024-07-01 14:34:02 +0900

    watchlist css 작업

--- src/Store/store.ts
+++ src/Store/store.ts
@@ -3,9 +3,11 @@ import { create } from "zustand";
 type TStore = {
   isShow: boolean;
   setIsShow: (isShow: boolean) => void;
+  watchList: number[];
 };
 
 export const useShow = create<TStore>()(set => ({
   isShow: false,
   setIsShow: isShow => set({ isShow }),
+  watchList: [],
 }));

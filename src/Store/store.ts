975405a5313b3ab4e839f4d95774a1b9e49b7eda
ur <dorosi150@gmail.com>
2024-07-01 16:47:25 +0900

    feat: login zutand 추가

--- src/Store/store.ts
+++ src/Store/store.ts
@@ -5,7 +5,18 @@ type TStore = {
   setIsShow: (isShow: boolean) => void;
 };
 
+type TLoginStore = {
+  isLoggedIn: boolean;
+  setLogin: () => void;
+};
+
 export const useShow = create<TStore>()(set => ({
   isShow: false,
   setIsShow: isShow => set({ isShow }),
+}));
+
+export const useLoginStore = create<TLoginStore>(set => ({
+  isLoggedIn: false,
+  setLogin: () => set({ isLoggedIn: true }),
+  // logout: () => set({ isLoggedIn: false }),
 }));

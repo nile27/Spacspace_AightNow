f35c53bcc5b0bfd87ae5233902750e2773e2a2d1
MS_btff <whdghks845@gmail.com>
2024-07-01 10:26:18 +0900

    watchlist css 작업

--- src/app/(watchlist)/watchlist/page.tsx
+++ src/app/(watchlist)/watchlist/page.tsx
@@ -1,10 +1,11 @@
 import Header from "@/components/Header";
 import WatchList from "@/features/Watchlist/components/WatchList";
+import WatchListAdd from "@/features/Watchlist/components/WatchListAdd";
 
 export default function page() {
   return (
     <>
-      <WatchList />
+      <WatchListAdd />
     </>
   );
 }

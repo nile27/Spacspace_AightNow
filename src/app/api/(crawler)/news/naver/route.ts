1f30f915d6d80e53bd71b5f0d8b44ee5ddd16cc1
ur <dorosi150@gmail.com>
2024-07-05 00:12:38 +0900

    fix: 네이버 전체 주식 뉴스 리스트 url 변경

--- src/app/api/(crawler)/news/naver/route.ts
+++ src/app/api/(crawler)/news/naver/route.ts
@@ -4,35 +4,27 @@ const symbols = ["AAPL.O", "TSLA.O", "MSFT.O", "AMZN.O", "GOOGL.O", "U"];
 
 type NewsData = {
   stockName?: string;
-  articleId: string;
-  officeId: string;
-  officeName: string;
-  datetime: string;
   type: number;
-  title: string;
-  body: string;
-  photoType: number;
-  imageOriginLink: string;
-  titleFull: string;
+  subcontent: string;
+  tumbUrl: string;
+  oid: string;
+  ohnm: number;
+  aid: string;
+  tit: string;
+  dt: number;
 };
 
 const fetchNaverNewsInfo = async (symbol: string): Promise<NewsData[]> => {
-  const url = `https://api.stock.naver.com/news/integration/${symbol}`;
+  const url = `https://api.stock.naver.com/news/worldStock/${symbol}?pageSize=20&page=1`;
   const headers = {
     "Content-Type": "application/json",
   };
 
   try {
-    const newsList: NewsData[] = [];
     const response = await fetch(url, { headers });
     const data = await response.json();
-    data.stockNews.forEach((news: any) => {
-      news.items.forEach((item: NewsData) => {
-        item.stockName = symbol;
-        newsList.push(item);
-      });
-    });
-    return newsList;
+
+    return data;
   } catch (error) {
     console.error(`Error fetching news for ${symbol}:`, error);
     return [];
@@ -45,7 +37,7 @@ export async function GET(request: Request) {
   for (let symbol of symbols) {
     const newsListData = await fetchNaverNewsInfo(symbol);
     if (newsListData) {
-      allStockNews.push(...newsListData); // Spread operator to push individual items
+      allStockNews.push(...newsListData);
     }
   }
 

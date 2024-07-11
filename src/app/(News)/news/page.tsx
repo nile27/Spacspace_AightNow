026d678ae28972c5ab1572dc194b3e7cdafe93e7
ur <dorosi150@gmail.com>
2024-07-11 12:07:49 +0900

    feat: 뉴스 리스트 연동

--- src/app/(News)/news/page.tsx
+++ src/app/(News)/news/page.tsx
@@ -1,9 +1,29 @@
+"use client";
+
+import { TNewsList } from "@/app/api/(crawler)/type";
 import Card from "@/components/Card/Card";
 import Header from "@/components/Header";
 import ListNews from "@/components/List/ListNews";
 import FavoriteNews from "@/features/report/components/FavoriteNews";
+import { useNewsStore } from "@/Store/newsStore";
+import Link from "next/link";
+import { useEffect, useState } from "react";
 
 export default function News() {
+  const [loading, setLoading] = useState(false);
+  const fetchNewsList = useNewsStore(state => state.fetchNewsList);
+  const data = useNewsStore(state => state.newsList);
+
+  useEffect(() => {
+    const loadInitialData = () => {
+      setLoading(true);
+      fetchNewsList();
+      setLoading(false);
+    };
+
+    loadInitialData();
+  }, [fetchNewsList]);
+
   return (
     <>
       <Header />
@@ -21,10 +41,18 @@ export default function News() {
         <h2 className="font-bold text-2xl py-4">최신뉴스 테스트 </h2>
         <div className="w-[1200px] h-[940px] bg-white rounded-2xl p-12">
           <div className="w-[1200px] h-[940px] flex flex-col gap-8 ">
-            <ListNews />
-            <ListNews />
-            <ListNews />
-            <ListNews />
+            {data.map(news => (
+              // <Suspense key={news.id} fallback={<NewsListSkeleton />}>
+              <Link
+                href={{
+                  pathname: `/news/${news.id}`,
+                }}
+                key={news.id}
+              >
+                <ListNews data={news} />
+              </Link>
+              // </Suspense>
+            ))}
           </div>
         </div>
       </div>

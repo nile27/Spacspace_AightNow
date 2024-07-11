b0dd0199061cb544d69cc6be6891bfaf9875e8e7
nile27 <ddj03104@gmail.com>
2024-07-12 00:08:32 +0900

    feat: loading 페이지  추가

---
+++ src/app/loading.tsx
@@ -0,0 +1,22 @@
+const loading = () => {
+  return (
+    <div
+      className={`bg-background m-0 p-0 h-[100%] w-[100%] flex justify-center items-center relative`}
+    >
+      <div
+        className={`bg-white rounded-xl m-0 p-0 h-[300px] w-[300px] flex justify-center items-center relative`}
+      >
+        <div className="flex gap-3 text-H text-black relative">
+          <span className=" animate-[blur_3s_infinite_0ms]">L</span>
+          <span className=" animate-[blur_3s_infinite_200ms]">O</span>
+          <span className=" animate-[blur_3s_infinite_400ms]">A</span>
+          <span className="animate-[blur_3s_infinite_800ms]">D</span>
+          <span className=" animate-[blur_3s_infinite_1200ms]">E</span>
+          <span className=" animate-[blur_3s_infinite_1400ms]">R</span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default loading;

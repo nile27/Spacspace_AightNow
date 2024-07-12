72f8ef45a13f9fd13b8cf353f9623ecad85a58ac
ur <dorosi150@gmail.com>
2024-07-12 16:22:55 +0900

    style: 뉴스 리스트 UI 수정
    이미지 없으면 글만 나옴

--- src/components/List/ListNews.tsx
+++ src/components/List/ListNews.tsx
@@ -1,19 +1,8 @@
 import { TNewsList } from "@/app/api/(crawler)/type";
+import { extractTextFromHTML, formatRelativeTime } from "@/features/news/components/common";
 
 type TListNews = {
   data: TNewsList;
-};
-
-export const extractTextFromHTML = (htmlString: string) => {
-  const parser = new DOMParser();
-  const doc = parser.parseFromString(htmlString, "text/html");
-
-  // 불필요한 태그 및 텍스트 제거
-  const styleTags = doc.querySelectorAll("style");
-  styleTags.forEach(tag => tag.remove());
-
-  const textContent = doc.body.innerText || doc.body.textContent || "";
-  return textContent.replace(/[\n\r]+/g, " ").trim(); // 줄바꿈 제거 및 트림 처리
 };
 
 export default function ListNews(props: TListNews) {
@@ -21,19 +10,21 @@ export default function ListNews(props: TListNews) {
   const subcontent = extractTextFromHTML(data.content);
   return (
     <>
-      <div className="w-[1104px] h-[212px] justify-start items-start gap-5 inline-flex">
+      <div className="justify-start mb-8 items-start gap-5 inline-flex">
         {data.hasImage ? (
           <img className="w-[252px] h-[148px] rounded-2xl" src={data.thumbUrl} />
         ) : (
-          <div className="w-[252px] h-[148px] bg-white rounded-2xl" />
+          <></>
+          // <div className="w-[252px] h-[148px] bg-white rounded-2xl" />
         )}
         <div className="flex-col justify-start items-start gap-4 inline-flex">
-          <div className="w-[832px] h-7 self-stretch justify-start items-center gap-4 inline-flex">
-            <div className="w-[82px] h-14 flex justify-between items-center grow shrink basis-0 text-neutral-900 text-lg font-bold font-['Pretendard'] leading-7">
-              {data.tit}
+          <div className="h-7 self-stretch justify-start items-center inline-flex">
+            <div className=" flex justify-between items-center grow shrink basis-0 text-neutral-900 text-lg font-bold font-['Pretendard'] leading-7">
+              {/* {data.tit} */}
+              <div dangerouslySetInnerHTML={{ __html: data.tit }} />
               <div className="justify-start items-start gap-2 flex">
                 <div className="text-zinc-600 text-sm font-medium font-['Pretendard'] leading-tight">
-                  n시간전
+                  {formatRelativeTime(data.dt)}
                 </div>
                 <div className="text-zinc-600 text-sm font-medium font-['Pretendard'] leading-tight">
                   ∙
@@ -44,10 +35,10 @@ export default function ListNews(props: TListNews) {
               </div>
             </div>
           </div>
-          <div className="w-[832px] h-14  text-neutral-900 text-base font-normal font-['Pretendard'] ">
+          <div className="h-15  text-neutral-900 text-base font-normal font-['Pretendard'] ">
             <p
               style={{
-                width: "832px",
+                // width: "832px",
                 overflow: "hidden",
                 textOverflow: "ellipsis",
                 display: "-webkit-box",

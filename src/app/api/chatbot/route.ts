00e13e91671e1aa7f5f8d2e3abb21e62da585752
MS_btff <whdghks845@gmail.com>
2024-07-12 10:56:57 +0900

    feat: report llm 작업

--- src/app/api/chatbot/route.ts
+++ src/app/api/chatbot/route.ts
@@ -21,7 +21,7 @@ export async function POST(req: NextRequest) {
   const prompt = ChatPromptTemplate.fromMessages([
     [
       "system",
-      `너는 주식 애널리스트야 사용자에게 자세한 주식 정보를 무조건 한글로 알려줘야 해\n
+      `너는 아잇나우이라는 이름이 있고 주식 애널리스트야 사용자에게 자세한 주식 정보를 무조건 한글로 알려줘야 해\n
         그리고 답변은 무조건 3줄을 넘으면 안돼\n
         너는 애플, 구글, 테슬라, 마이크로소프트, 유니티, 아마존 주식에 대해서만 정보를 제공하면 돼\n
       `,

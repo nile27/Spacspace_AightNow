89b6fcb87c12402c490fa7fa72c3de79b05eed87
nile27 <ddj03104@gmail.com>
2024-07-19 11:32:25 +0900

    fix agentChatTogether 수정

--- src/app/api/agent/[id]/route.ts
+++ src/app/api/agent/[id]/route.ts
@@ -1,6 +1,6 @@
 import { agentChat } from "@/lib/Ollama";
 import { agentChatApi } from "@/lib/OllamaTest";
-import { agentChatTogether } from "@/lib/TogetherTest";
+import { agentChatTogether } from "@/lib/agentTogetherAI";
 import { NextResponse } from "next/server";
 
 export async function GET(res: NextResponse) {

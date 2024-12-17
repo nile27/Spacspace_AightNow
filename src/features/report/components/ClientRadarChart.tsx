5298b228a6738f84cdc6add96535e3a8a1fcd927
nile27 <ddj03104@gmail.com>
2024-12-17 20:58:44 +0900

    fix: 발견 페이지 레이아웃 겹침 현상 수정

--- src/features/report/components/ClientRadarChart.tsx
+++ src/features/report/components/ClientRadarChart.tsx
@@ -8,9 +8,8 @@ type ClientRadarChartProps = {
 
 export default function ClientRadarChart({ stockName }: ClientRadarChartProps) {
   return (
-    <div className="w-[429px] h-[297px] bg-white rounded-2xl p-4">
-      <div className="font-['pretendard'] font-bold text-2xl">종목 AI 리포트 점수</div>
+    <>
       <RadarChart stockName={stockName} width={400} />
-    </div>
+    </>
   );
 }
